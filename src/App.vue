924884f02d9b9da8214ae3f66930bbae7f4104e9
meet-vaghasiya <meetvaghasiya2012@gmail.com>
2021-05-27 22:29:34 +0530

    navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="content">
+    <navbar />
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style lang="scss">
 .content {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/ninja.png" alt="" />
+      <h1>
+        <router-link :to="{ name: 'Home' }">Muso Ninjas</router-link>
+      </h1>
+      <div class="links">
+        <button>Logout</button>
+
+        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: #fff;
+}
+nav {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 50px;
+}
+</style>
